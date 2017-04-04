e849ba516d63d1150ef98b561f7ab356cbc15693
Isaac <isaac.tegeler@gmail.com>
2017-04-04 11:48:52 -0500

    heat map now working

--- localize.py
+++ localize.py
@@ -2,11 +2,15 @@ from keras import applications
 from keras.models import Sequential
 from keras.preprocessing import image
 from keras.applications.vgg16 import preprocess_input
-import numpy as np
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+fullNet = Sequential()
 # build the VGG16 network
-model = applications.VGG16(include_top=True,
+model = applications.VGG16(include_top=False,
                            weights='imagenet')
+
 
 #prepare image for prediction
 img_path = 'catTest.jpeg'
@@ -20,9 +24,33 @@ layer_dict = dict([(layer.name, layer) for layer in model.layers])
 
 
 features = model.predict(x)
+
+'''
+this should find the max in the x piece, and in the y piece of the prediction output
+so that I can create a heat map of the global maximum in those dimensions
+'''
+def createMaxGrid(arr):
+	globalMaxes = np.zeros(shape=(7,7))
+	currElementMax = 0
+	for i in range (0, 7):
+		lineMaxes = np.zeros(shape=(7))
+		for j in range(0, 7):
+			for k in range(0, 512):
+				if (features[0, i, j, k] > currElementMax):
+					currElementMax = features[0, i, j, k]
+			lineMaxes[j] = currElementMax
+			currElementMax = 0
+		globalMaxes[i] = lineMaxes
+	return globalMaxes
+
+plt.imshow(createMaxGrid(features), cmap='hot', interpolation='nearest')
+plt.show()
+
+'''
+print(features.shape)
 print('Predicted:', features)
 
-
+'''
 
 
 
