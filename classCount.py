a39092e2e770bff4a0107702e2d5acd6af87a08f
Isaac <isaac.tegeler@gmail.com>
2017-04-14 15:55:27 -0500

    first build

---
+++ classCount.py
@@ -0,0 +1,11 @@
+import xml.etree.ElementTree as ET
+import numpy as np
+
+x = np.zeros(shape=(100000))
+
+for i in range (0, 100000)
+	tree = ET.parse('%d.xml' (i))
+	root = tree.getroot()
+	image = root[0]
+	x[i] = 
+	
