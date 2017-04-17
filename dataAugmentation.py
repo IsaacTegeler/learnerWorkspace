c5700cbd2a5546e77f811c6655633fd7f126e2a1
Isaac <isaac.tegeler@gmail.com>
2017-04-17 14:51:42 -0500

    changed director setup

--- dataAugmentation.py
+++ dataAugmentation.py
@@ -1,6 +1,20 @@
 from keras.preprocessing.image import ImageDataGenerator
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 
+'''
+directory set up
+	/data
+		/training data
+			
+			/cats
+				cat_0.jpg
+				cat_1.jpg
+				cat_*.jpg
+			/dogs
+				dog_0.jpg
+				dog_1.jpg
+				dog_*.jpg
+'''
 datagen = ImageDataGenerator(
 		rotation_range=40,
 		width_shift_range=0.2,
@@ -10,7 +24,7 @@ datagen = ImageDataGenerator(
 		zoom_range=0.2,
 		horizontal_flip=True,
 		fill_mode='nearest')
-img = load_img('data/cats/cat.0.jpg')  # this is a PIL image
+img = load_img('data/trainingData/cats/cat.0.jpg')  # this is a PIL image
 x = img_to_array(img)  # this is a Numpy array with shape (3, 150, 150)
 x = x.reshape((1,) + x.shape)  # this is a Numpy array with shape (1, 3, 150, 150)
 
