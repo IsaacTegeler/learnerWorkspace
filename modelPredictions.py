9ed651f9238c18607b0874b1c3ea841ab13ab68b
Isaac Tegeler <isaac.tegeler@gmail.com>
2017-09-06 10:19:26 -0500

    Updated for new Keras API

--- modelPredictions.py
+++ modelPredictions.py
@@ -5,23 +5,19 @@ from keras import backend as K
 from  keras.applications.imagenet_utils import preprocess_input
 from keras.callbacks import LearningRateScheduler
 from keras.layers import GlobalAveragePooling2D
-import matplotlib.pyplot as plt
+
 import numpy as np
 K.set_image_dim_ordering('th')
 
 #-----------------pre building for size, shape, and defined constants-------------------------------
-img_width, img_height = 300, 300
+img_width, img_height = 150, 150
 count = 1
 lossChange = 0.01
 bestLossIndex = 0
 batch_size = 1
 
-model = Sequential()
-model.add(ZeroPadding2D((1, 1) , batch_input_shape=(batch_size, 3, img_width, img_height)))
-first_layer = model.layers[-1]
-
-img_path = 'catTest.jpeg'
-img = load_img(img_path, target_size=(300, 300))
+img_path = 'testData/dogTest.jpeg'
+img = load_img(img_path, target_size=(img_width, img_height))
 x = img_to_array(img)
 x = x.reshape((1,) + x.shape)
 
@@ -36,19 +32,27 @@ model.add(ZeroPadding2D((1, 1) , batch_input_shape=(batch_size, 3, img_width, im
 #---------------------Network Architecture--------------------------------------------------------
 
 #Convolution layers -------------
-model.add(Convolution2D(32, 3, 3, activation = 'relu', name = 'con_1'))
+model.add(Convolution2D(32, (3, 3), activation = 'relu', name = 'con_1'))
 model.add(MaxPooling2D((2,2), strides=(2,2)))
 model.add(ZeroPadding2D((1, 1)))
-model.add(Convolution2D(32, 3, 3, activation = 'relu', name = 'con_2'))
+model.add(Convolution2D(32, (3, 3), activation = 'relu', name = 'con_2'))
 model.add(MaxPooling2D((2,2), strides=(2,2)))
 model.add(ZeroPadding2D((1, 1)))
 
 
-
-model.add(Convolution2D(32, 3, 3, activation = 'relu', name = 'con_3'))
+model.add(Convolution2D(64, (3, 3), activation = 'relu', name = 'con_3'))
 model.add(MaxPooling2D((2,2), strides=(2,2)))
 model.add(ZeroPadding2D((1, 1)))
-model.add(Convolution2D(32, 3, 3, activation = 'relu', name = 'con_4'))
+model.add(Convolution2D(64, (3, 3), activation = 'relu', name = 'con_4'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+model.add(ZeroPadding2D((1, 1)))
+
+#Fully connected layers ---------
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(64, activation = 'relu'))
+model.add(Dropout(0.5))
+model.add(Dense(2, activation = 'sigmoid', name='dense_3'))
 
 
 #------------------look at the layers------------------------------------------------------------
@@ -56,7 +60,7 @@ layer_dict = dict([(layer.name, layer) for layer in model.layers])
 
 import h5py
 
-weights_path = 'trained_net.h5'
+weights_path = 'trained_net_2.h5'
 
 model.load_weights(weights_path, by_name=True)
 
@@ -65,5 +69,8 @@ print('Model loaded.')
 #predict the class of the image
 preds = model.predict(x, verbose=1)
 print(preds.shape)
-print(preds)
+if(preds[0][0] == 1):
+	print('cat')
+else:
+	print('dog')
 
